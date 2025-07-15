91fa65b032121c0db80dc2501c73c0eaed835bdb
lucasagostini <lbagostini@inf.ufpel.edu.br>
2025-07-15 19:52:29 -0300

    Adds views and url for app

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from .models import BlogPost, Comment
+from .serializers import BlogPostSerializer, CommentSerializer
 
-# Create your views here.
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+    
+    @action(detail=True, methods=['post'], url_path='comments')
+    def create_comment(self, request, pk:None):
+        post = self.get_object()
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(post=post)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
