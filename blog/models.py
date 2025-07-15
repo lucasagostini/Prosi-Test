4b595e3a0b0115c8c1f7112668f6611c27e1dae5
lucasagostini <lbagostini@inf.ufpel.edu.br>
2025-07-15 18:20:23 -0300

    Adds models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField()
+    content = models.TextField()
+    
+    def __str__(self):
+        return self.title
+    
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        BlogPost,
+        related_name="comments",
+        on_delete=models.CASCADE
+    )
+    author = models.CharField()
+    content = models.TextField()
+    
+    
+    def __str__(self):
+        return f"{self.author} on {self.post.title}"
